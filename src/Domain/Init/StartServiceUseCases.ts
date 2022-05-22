a4681293fee94bc5627e7268c38baa82b77e37fd
Mauricio Agudelo <mauro.0116@gmail.com>
2022-05-21 20:34:01 -0500

    Fix payload limit

--- src/Domain/Init/StartServiceUseCases.ts
+++ src/Domain/Init/StartServiceUseCases.ts
@@ -11,8 +11,8 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit:'60mb'}));
+app.use(express.urlencoded({ extended: true, limit: '60mb' }));
 app.use(indexRoutes);
 
 async function init() {
