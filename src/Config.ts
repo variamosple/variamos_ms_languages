852889f5719399635a5e02e8d5c2de3013ed5a18
aolopezeafit <aolopezh@eafit.edu.co>
2023-09-29 12:20:02 -0500

    Env

--- src/Config.ts
+++ src/Config.ts
@@ -12,12 +12,12 @@ export const Config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     HOST: process.env.HOST || '127.0.0.1',
     PORT: process.env.PORT || 4000,
-    VERSION: "1.23.09.29.11",
+    VERSION: "1.23.09.29.1219",
     DB: {
-      USER: process.env.DB_USER || "DB_USER",
-      HOST: process.env.DB_HOST || "DB_HOST",
-      PASSWORD: process.env.DB_PASSWORD || "DB_PASSWORD",
-      DATABASE: process.env.DB_DATABASE || "DB_DATABASE",
+      USER: process.env.DB_USER || "db_user_test",
+      HOST: process.env.DB_HOST || "db_host_test",
+      PASSWORD: process.env.DB_PASSWORD || "db_password_test",
+      DATABASE: process.env.DB_DATABASE || "db_database_test",
       PORT: process.env.DB_PORT || 5432,
       SSL: process.env.DB_SSL || true
     }
